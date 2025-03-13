3b0e2b33bf8627b79b3bfdd608399ec02856abfa
Rafael-aaf <126685313+Rafael-aaf@users.noreply.github.com>
2025-03-13 10:33:36 -0300

    Add files via upload

---
+++ Fila Estática.cpp
@@ -0,0 +1,58 @@
+// Programa Fila em C
+
+#include<stdio.h>
+#include<conio.h>
+
+//#define tamVetor 3
+//int vetor[tamVetor];
+
+
+int main(){
+	int cont = 0; //contador
+	int vetor[3];
+	
+	while (true){
+		int n;
+		
+		printf("\n\tPrograma Fila em C++\n\n");
+		printf("Para inserir digite\t 1\n");
+		printf("Para remover digite\t 2\n");
+		printf("Para exibir digite\t 3\n\n");
+		printf("Para sair\t\t 4\n\n");
+		scanf("%d", &n);
+		
+		switch (n){
+			case 1:
+			if (cont >= 3){
+				printf("\nA fila esta cheia\n");
+				continue;
+			}
+			printf("\nDigite um numero para ser inserido: ");
+			scanf("%d", &vetor[cont]);
+			
+			cont++;
+			break;
+		
+		case 2:
+			for(int i= 0; i < cont; i++){
+				vetor[i] = vetor[i + 1];
+			}
+			cont--;
+			break;
+		
+		case 3:
+			if (cont < 1){
+				printf("\nA fila esta vazia\n");
+			}else{
+			printf("\nO numero exibido: \t");
+			for (int i = 0; i < cont; i++){
+				printf("\n%d", vetor[i]);
+			}
+			}//printf("%d\n\n", vetor[cont - 1]);}
+			break;
+		
+		case 4:
+			return 0;
+		}
+	}
+}
